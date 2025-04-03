f03776639563afbec7f86c1d3a93c1235d8db7d1
Murilo <Murisuzuki11@gmail.com>
2025-04-03 20:41:53 -0300

    Void usuario

--- usuario.dart
+++ usuario.dart
@@ -1,5 +1,14 @@
+
+import 'Livro.dart';
+
+
 void Usuario() {
   String nome;
   List<Livro> livrosEmprestados = [];
   // [] mostra que a lista esta vazia 
+  Usuario(this.nome);
+
+  void pegarLivroEmprestado(Livro livro){
+
+  }
  }
