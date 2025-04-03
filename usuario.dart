79953fe4da1361cfcb554f3302d4ebb1084e18be
Murilo <Murisuzuki11@gmail.com>
2025-04-03 20:53:20 -0300

    Finaliazando parcialmente projeto

--- usuario.dart
+++ usuario.dart
@@ -5,10 +5,16 @@ import 'Livro.dart';
 void Usuario() {
   String nome;
   List<Livro> livrosEmprestados = [];
-  // [] mostra que a lista esta vazia 
   Usuario(this.nome);
+    // [] mostra que a lista esta vazia 
 
   void pegarLivroEmprestado(Livro livro){
-
+    if(livro.emprestado == false){  
+      livrosEmprestados.add(livro);
+      livro.emprestado = true;
+      print("\ $nome Pegou o livro \ ${livro.titulo} emprestado");
+    }else{
+      print('O livro \ ${livro.titulo} ja foi emprestado');
+    }
   }
  }
