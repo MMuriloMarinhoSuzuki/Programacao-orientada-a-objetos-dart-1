042e25074a7313bc1bcb81f24ff7b3613b8e6578
2Bmodular <2bmod@bglab.local>
2025-04-08 22:21:33 -0300

    Adicionando livros e usuarios na biblioteca

--- POO.dart
+++ POO.dart
@@ -1,6 +1,13 @@
 import 'Livro.dart';
+import 'biblioteca.dart';
+import 'usuario.dart';
 
 void main() {
+  Biblioteca biblioteca = new Biblioteca();
   Livro livro = new Livro("1984", "George Orwell", 1950);
-  livro.exibirDetalhes();
+  Livro livro2 = new Livro("O senhor dos aneis", "J.R Tolkien", 1950);
+  // livro.exibirDetalhes();
+  Usuario usuario1 = new Usuario("Murilo");
+  Usuario usuario2 = new Usuario("Francisco");
+  biblioteca.cadastrarUsuario(usuario1);
 }
