91925f0ca5e83501949e3a1145f6229887fc90fd
2Bmodular <2bmod@bglab.local>
2025-04-08 21:42:16 -0300

    corrigindo erros

--- usuario.dart
+++ usuario.dart
@@ -1,20 +1,28 @@
-
 import 'Livro.dart';
 
-
-void Usuario() {
+class Usuario() {
   String nome;
   List<Livro> livrosEmprestados = [];
   Usuario(this.nome);
-    // [] mostra que a lista esta vazia 
+  // [] mostra que a lista esta vazia
 
-  void pegarLivroEmprestado(Livro livro){
-    if(livro.emprestado == false){  
+  void pegarLivroEmprestado(Livro livro) {
+    if (livro.emprestado == false) {
       livrosEmprestados.add(livro);
       livro.emprestado = true;
       print("\ $nome Pegou o livro \ ${livro.titulo} emprestado");
-    }else{
+    } else {
       print('O livro \ ${livro.titulo} ja foi emprestado');
     }
   }
- }
+
+  void devolverLivro(Livro livro) {
+    if (livrosEmprestados.contains(livro)) {
+      livrosEmprestados.remove(Livro);
+      livro.emprestado = false;
+      print('$nome devolveu o livro ${livro.titulo}');
+    } else {
+      print('$nome não possui o livro ${livro.titulo} para devolver');
+    }
+  }
+}
