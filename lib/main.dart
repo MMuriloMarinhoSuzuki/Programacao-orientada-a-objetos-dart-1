b79f428b1b224dc514ec5e3ab659a2ef77ac2e31
2Bmodular <2bmod@bglab.local>
2025-04-10 21:57:34 -0300

    COmeçando a criar a interface!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MinhaClasse(),));
+  runApp(MaterialApp(home: MinhaClasse()));
+}
+
+class MinhaClasse extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Aula 1"), 
+        backgroundColor: Colors.amber),
+        body: Center(child:Text("Olá Mundo!")),
+    );
+  }
 }
