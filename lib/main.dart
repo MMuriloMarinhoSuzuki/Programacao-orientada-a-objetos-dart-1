f6403577d1d340870abf086de80db9044fda3735
2Bmodular <2bmod@bglab.local>
2025-04-15 21:14:34 -0300

    Começando a criação da tela e emulando no browser

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,8 @@ class MinhaClasse extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("App Aula 1"), 
-        backgroundColor: Colors.amber),
-        body: Center(child:Text("Olá Mundo!")),
+      appBar: AppBar(title: Text("App Aula 1"), backgroundColor: Colors.amber),
+      body: Center(child: Text("Olá Mundo!")),
     );
   }
 }
